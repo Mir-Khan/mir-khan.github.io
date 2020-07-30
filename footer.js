12b3739eaca11d9afb32bde02dbee450c0bbecd7
Mir-Khan <mirkhan1497@gmail.com>
2020-07-30 03:35:48 -0400

    Fixed the react even more, silly mistake :(

--- footer.js
+++ footer.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
-class Footer extends React.Component{
-    render(){
-        <div id="footer">    
-            <p id="footer-text">Find me at the links below!</p>
-            <a href="https://github.com/Mir-Khan" target="_blank" id="github-link"><i class="fab fa-github fa-3x"></i></a>
-            <a href="https://www.linkedin.com/in/mir-khan/" target="_blank" id="linkedin-link"><i class="fab fa-linkedin fa-3x"></i></a>
-        </div>
+class Footer extends React.Component {
+    render() {
+        return (
+            <div id="footer">
+                <p id="footer-text">Find me at the links below!</p>
+                <a href="https://github.com/Mir-Khan" target="_blank" id="github-link"><i class="fab fa-github fa-3x"></i></a>
+                <a href="https://www.linkedin.com/in/mir-khan/" target="_blank" id="linkedin-link"><i class="fab fa-linkedin fa-3x"></i></a>
+            </div>
+        )
     }
 }
 
-ReactDOM.render(<Footer/>, document.getElementById("page-footer"));
+ReactDOM.render(<Footer />, document.getElementById("page-footer"));
