37647f200239249c2e3bbb189c7aef5271552df0
Mir-Khan <mirkhan1497@gmail.com>
2020-07-29 21:59:27 -0400

    commit on master

--- d3-bar/index.js
+++ d3-bar/index.js
@@ -6,13 +6,7 @@ const h = 100;
 //data source
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 //requesting the data
-document.addEventListener('DOMContentLoaded', function(){
-    const req = new XMLHttpRequest();
-    req.open('GET', url, true);
-    req.send();
-    req.onload = function(){
-        const json = JSON.parse(req.responseText);
-        console.log(json);
-    }
-})
+d3.json(url, function(data){
+    
+});
 const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
